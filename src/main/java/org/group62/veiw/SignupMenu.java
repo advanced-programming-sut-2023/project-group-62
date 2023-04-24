fcd8779c2665128459001849689aafa265facc92
AmirHossein <aminianfar.amir@gmail.com>
2023-04-24 16:21:39 +0430

    master: creat SignupMenu constructor and run method

--- src/main/java/org/group62/veiw/SignupMenu.java
+++ src/main/java/org/group62/veiw/SignupMenu.java
@@ -1,4 +1,21 @@
 package org.group62.veiw;
 
+import java.util.Scanner;
+
 public class SignupMenu {
+    SignupMenuController signupMenuController;
+    LoginMenuController loginMenuController;
+    public SignupMenu(SignupMenuController signupMenuController){
+        this.signupMenuController = signupMenuController;
+        loginMenuController = new LoginMenuController();
+    }
+
+    Scanner scanner = new Scanner(System.in);
+    String inputCommand;
+    public void run(){
+        while (true){
+            inputCommand = scanner.nextLine();
+
+        }
+    }
 }
