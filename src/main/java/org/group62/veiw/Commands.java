4b6b862a264f0988efcbb569764113cf51a6bd61
AmirHossein <aminianfar.amir@gmail.com>
2023-04-24 10:27:59 +0430

    master: create Commands constructor and getMatcher

--- src/main/java/org/group62/veiw/Commands.java
+++ src/main/java/org/group62/veiw/Commands.java
@@ -1,4 +1,18 @@
 package org.group62.veiw;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public enum Commands {
+    private String regex;
+    private Commands(String regex){
+        this.regex = regex;
+    }
+    public Matcher getMatcher(String input,Commands mainRegex){
+        Matcher matcher = Pattern.compile(mainRegex.regex).matcher(input);
+        if(matcher.matches())
+            return matcher;
+        else
+            return null;
+    }
 }
