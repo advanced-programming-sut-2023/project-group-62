1a7107f18e769977946209599fcfe1ba68a4111d
AmirHossein <aminianfar.amir@gmail.com>
2023-04-25 07:57:45 +0430

    AmirHossein: edit main Menu

--- src/main/java/org/group62/veiw/MainMenu.java
+++ src/main/java/org/group62/veiw/MainMenu.java
@@ -1,4 +1,33 @@
 package org.group62.veiw;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+
 public class MainMenu {
+    ProfileMenuController profileMenuController;
+    GameMenuController gameMenuController;
+    MainMenuController mainMenuController;
+    Matcher matcher;
+    public MainMenu(MainMenuController mainMenuController){
+        this.mainMenuController = mainMenuController;
+        gameMenuController = new GameMenuController;
+        profileMenuController = new ProfileMenuController;
+    }
+    public void run(Scanner scanner){
+        while (true){
+            String inputCommand = scanner.nextLine();
+            if(Commands.getMatcher(inputCommand,Commands.ENTER_PROFILE_MENU) != null){
+                ProfileMenu profileMenu = new ProfileMenu(profileMenuController);
+                profileMenu.run(scanner);
+            }
+            else if(Commands.getMatcher(inputCommand,Commands.ENTER_GAME_MENU) != null){
+                GameMenu gameMenu = new GameMenu(gameMenuController);
+                gameMenu.run(scanner);
+            }
+            else if(Commands.getMatcher(inputCommand,Commands.BACK) != null)
+                break;
+            else
+                System.out.println("Invalid command!");
+        }
+    }
 }
