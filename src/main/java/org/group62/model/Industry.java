d93cac1304666e32b656dc2e54bd5e1675d47f6f
AmirHossein <aminianfar.amir@gmail.com>
2023-05-13 14:24:16 +0330

    add model

--- src/main/java/org/group62/model/Industry.java
+++
@@ -1,9 +0,0 @@
-package org.group62.model;
-
-import java.util.HashMap;
-
-public class Industry extends Building {
-    public Industry(Governance owner, String name, int goldCost, HashMap<Resource, Integer> resourcesCost, int workersNumber, HashMap<Constant, Integer> constants) {
-        super(owner, name, goldCost, resourcesCost, workersNumber, constants);
-    }
-}
--- src/main/java/org/group62/model/Worker.java
+++
@@ -1,7 +0,0 @@
-package org.group62.model;
-
-public class Worker extends People {
-    public Worker(Governance owner) {
-        super(owner);
-    }
-}
