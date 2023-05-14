88f5da980fed42a564eabbeb6d9000603f8c6a5a
AmirHossein <aminianfar.amir@gmail.com>
2023-05-14 22:07:23 +0330

    create market menu

--- src/main/java/org/group62/model/Resource.java
+++ src/main/java/org/group62/model/Resource.java
@@ -1,5 +1,27 @@
 package org.group62.model;
 
 public enum Resource {
-    WOOD, STONE, IRON, PITCH, BEER, FLOUR, WHEAT, BARLEY;
+    WOOD(25,10),
+    STONE(40,20),
+    IRON(60,30),
+    PITCH(35,15),
+    BEER(30,15),
+    FLOUR(40,20),
+    WHEAT(25,10),
+    BARLEY(25,10);
+    private int buyPrice;
+    private int sellPrice;
+
+    private Resource(int buyPrice, int sellPrice) {
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+    }
+
+    public int getBuyPrice() {
+        return buyPrice;
+    }
+
+    public int getSellPrice() {
+        return sellPrice;
+    }
 }
